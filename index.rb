0a39641cd866efe9fc343db8fcb3ce61c56616ea
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-20 00:14:34 +0100

    challenge solution

--- index.rb
+++ index.rb
@@ -1,12 +1,15 @@
 # Given an array, how many items are greater than 5?
 
+def how_many_items_are_greaer_than(number)
+    for item in array
+        if item is > 5
+            count = count + 1
+        end
+    end
+    return count
+end
+
 array =[17, 6, 3, 7, 9, 1]
-number = 5
 count = 0
 
-for item in array
-    if item is > 5
-        count = count + 1
-    end
-end
-puts count
+puts how_many_items_are_greaer_than(5)
