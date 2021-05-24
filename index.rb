27b2a03ea2c51c6929c60df38004b76afb1512e4
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-24 19:15:46 +0100

    add array lessons

--- index.rb
+++ index.rb
@@ -58,3 +58,16 @@ num_array.pop(3)          #=> [4, 5, 6]
 num_array.shift(2)        #=> [1, 2]
 num_array                 #=> [3]
 
+Adding and Subtracting Arrays
+What do you think will be the outcome of [1, 2, 3] + [3, 4, 5]?
+
+If you guessed [1, 2, 3, 3, 4, 5], congratulations! Adding two arrays will return a new array built by concatenating them, similar to string concatenation. The concat method works the same way.
+
+a = [1, 2, 3]
+b = [3, 4, 5]
+
+a + b         #=> [1, 2, 3, 3, 4, 5]
+a.concat(b)   #=> [1, 2, 3, 3, 4, 5]
+To find the difference between two arrays, you can subtract them using -. This method returns a copy of the first array, removing any elements that appear in the second array.
+
+[1, 1, 1, 2, 2, 3, 4] - [1, 4]  #=> [2, 2, 3]
