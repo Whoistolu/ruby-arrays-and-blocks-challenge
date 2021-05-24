7d54a03b1e99fea4fb83d5dd40e989dcd5253471
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-24 19:12:10 +0100

    add array lessond

--- index.rb
+++ index.rb
@@ -1,15 +1,17 @@
-# Given an array, how many items are greater than 5?
+Creating Arrays
 
-def how_many_items_are_greaer_than number, array
-    count = 0
-    for item in array
-        if item is > 5
-            count = count + 1
-        end
-    end
-    return count
-end
+Here are two basic arrays:
 
+num_array = [1, 2, 3, 4, 5]
+str_array = ["This", "is", "a", "small", "array"]
 
-array =[17, 6, 3, 7, 9, 1]
-puts how_many_items_are_greaer_than(5, array)
+Both arrays have five elements separated by commas. The first array contains integers, while the second array contains strings.
+
+Arrays are commonly created with an array literal, which is simply a special syntax used to create instances of an array object. To create a new array using an array literal, use square brackets ([]).
+
+An array can also be created by calling the Array.new method. When you call this method, you can also include up to 2 optional arguments (initial size and default value):
+
+Array.new               #=> []
+Array.new(3)            #=> [nil, nil, nil]
+Array.new(3, 7)         #=> [7, 7, 7]
+Array.new(3, true)      #=> [true, true, true]
